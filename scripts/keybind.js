db8b181c8a0de497c7e1c8f87732adbc4891270c
Erica <erica_wang@ucsb.edu>
2026-05-20 22:13:47 -0700

    Add fun facts

--- scripts/keybind.js
+++ scripts/keybind.js
@@ -3,5 +3,7 @@ $(document).on('keydown', function (event) {
         window.location.href = "about.html";
     } else if (event.which == 66) {
         window.location.href = "blog.html";
+    } else if (event.which == 77) {
+        window.location.href = "misc.html";
     }
 });
