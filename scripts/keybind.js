cc467f4d1cbfac6d23681b59ca9ba0034798076f
Erica <erica_wang@ucsb.edu>
2026-05-20 18:38:11 -0700

    Add main.css

---
+++ scripts/keybind.js
@@ -0,0 +1,7 @@
+$(document).on('keydown', function (event) {
+    if (event.which == 65) {
+        window.location.href = "about.html";
+    } else if (event.which == 66) {
+        window.location.href = "blog.html";
+    }
+});
