0bac0f6854ba494fbed9efefdba9c2d8f4a7673a
Erica <gnawacire@gmail.com>
2026-05-21 17:53:28 -0700

    Fix dark mode toggle

---
+++ scripts/colors.js
@@ -0,0 +1,28 @@
+const prefersDarkColorScheme = () =>
+  window &&
+  window.matchMedia &&
+  window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+$(document).ready(load);
+
+//setup listener for changing themes
+function load() {
+    const toggleSwitch = document.getElementById("colors-toggle");
+    if (localStorage.getItem("theme") === "light") {
+        document.documentElement.setAttribute('data-theme', 'light');
+    } else if (localStorage.getItem("theme") === "dark") {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        const target = prefersDarkColorScheme ? 'dark' : 'light';
+        document.documentElement.setAttribute('data-theme', target);
+        localStorage.setItem("theme", target);
+    }
+    toggleSwitch.addEventListener('click', switchTheme);
+}
+
+//function that changes the theme, and sets a localStorage variable to track the theme between page loads
+function switchTheme() {
+    const target = document.documentElement.getAttribute('data-theme') === 'light' ? 'dark' : 'light';
+    document.documentElement.setAttribute('data-theme', target);
+    localStorage.setItem("theme", target);
+}
